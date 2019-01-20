c2721135e380a91676357584cb3410bc03539e48
sigmaray <sigmaray@gmail.com>
2019-01-20 07:59:06 +0300

    lein new crandom

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to crandom
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/crandom/core_test.clj
@@ -0,0 +1,7 @@
+(ns crandom.core-test
+  (:require [clojure.test :refer :all]
+            [crandom.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
